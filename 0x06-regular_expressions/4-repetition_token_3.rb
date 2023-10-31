b2b79fb9f1fb42ff1a907b1ef571c9a9a1dec22f
root <okekefeargod@gmail.com>
2023-10-31 05:12:07 -0700

    3 and 4

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#+ Using the project instructions, create a Ruby script that accepts
+#+ one argument and pass it to a regular expression matching method
+
+def match_hbn(argument)
+  pattern = /(hbt{0,4}n)/
+  match = argument.scan(pattern)
+  if match.any?
+    puts match.join
+  else
+    puts ""
+  end
+end
+
+if ARGV.length < 1
+  puts "Please provide an argument."
+else
+  argument = ARGV[0]
+  match_hbn(argument)
+end
