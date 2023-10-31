71fdc2c6787b6987206da72f868ec2ac2e48567a
root <okekefeargod@gmail.com>
2023-10-31 06:28:36 -0700

    8. Textme 2

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,25 +4,4 @@
 #+ The receiver phone number or name (including country code if present)
 #+ The flags that were use
 
-if ARGV.length < 1
-  puts "Usage: #{$0} <logfile>"
-  exit(1)
-end
-
-logfile = ARGV[0]
-
-results = []
-
-File.readlines(logfile).each do |line|
-  if line =~ /Receive SMS|Sent SMS/
-    sender =  line.match(/\[from:(.*?)\]/)
-    receiver = line.match(/\[to:(.*?)\]/)
-    flags = line.match(/\[flags:(.*?)\]/)
-
-    if sender && receiver && flags
-      results << "#{sender[1]},#{receiver[1]},#{flags[1]}"
-    end
-  end
-end
-
-puts results
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
