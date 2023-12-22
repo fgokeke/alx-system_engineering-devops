f3cc1baad1703fb76dd67619a1a7b715e22e77a0
root <okekefeargod@gmail.com>
2023-12-22 09:17:44 -0800

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Description: Puppet manifest to install Flask version 2.1.0 using pip3.
+#Description: Puppet manifest to install Flask version 2.1.0 using pip3.
+
+exec { 'upgrade_werkzeug':
+  command => '/usr/bin/pip3 install --upgrade Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+}
+
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Exec['upgrade_werkzeug'],
 }
