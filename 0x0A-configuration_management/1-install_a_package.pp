80863acba2f4571f4c9bbf4f117baba8ef8ee43b
root <okekefeargod@gmail.com>
2023-12-22 09:08:14 -0800

    1. Install a package2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup
 # Description: Puppet manifest to install Flask version 2.1.0 using pip3.
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
