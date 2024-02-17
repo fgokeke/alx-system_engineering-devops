a93dcf416edd2622a2997c6b3bdbdbcc8a07bb53
root <okekefeargod@gmail.com>
2024-02-17 05:39:03 -0800

    1. Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,10 +19,12 @@ def number_of_subscribers(subreddit):
     Returns:
     int: The number of subscribers or 0 if the subreddit is invalid.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'MyApp/1.0.0 (http://myapp.example.com)'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.adv:v1.0.0 (by /u/fg_okeke)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
